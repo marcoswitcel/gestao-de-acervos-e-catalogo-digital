319808828555e4354b7fe2c4cd5daede6ad9a233
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-11-27 20:56:46 -0300

    .env

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
 /**
+ * https://cli.vuejs.org/guide/mode-and-env.html#environment-variables
+ */
+const urlBackend = import.meta.env.VITE_BACKEND_ENDPOINT || 'http://localhost:3000/';
+
+/**
  * Axios com as configurações apropriadas para requisições para a API (backend)
  * @link https://www.npmjs.com/package/axios#axioscreateconfig
  */
 export const $api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: urlBackend,
    // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
