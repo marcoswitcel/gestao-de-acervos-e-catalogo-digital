8ccee975c0226b1e6379291f0c6cbf2b0177a8c9
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-11-27 21:26:43 -0300

    redirect fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import { TokenManager } from '@/utilities';
 
 const router = createRouter({
@@ -8,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      redirect: 'dashboard'
     },
     {
       path: '/login',
